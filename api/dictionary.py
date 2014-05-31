8b5627bfdabb345e5d48b31b33f9978167d96e89
Jonathan Webb <jwebbed@outlook.com>
2014-05-31 19:37:28 -0400

    It sorta works

--- api/dictionary.py
+++ api/dictionary.py
@@ -1,17 +1,24 @@
 import urllib2
-import json
+import xml.etree.ElementTree as ET
+
 
 api_call = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/{0}?key=9baecd26-8583-4b74-9922-ce2a1170df9d"
 
 def query(word):
     f = urllib2.urlopen(api_call.format(word))
-    json_string = f.read()
+    xml_string = f.read()
     f.close()
-    return json.loads(json_string)
+    return ET.fromstring(xml_string)
 
-def defineWord(word):
-    parsed_json = query(word)
-    print "lol"
+def getDefinition(word):
+    parsed_xml = query(word)
+    entry = parsed_xml[0]
+    for node in entry:
+        if node.tag == 'def':
+            for other_node in node:
+                if other_node.tag == 'dt':
+                    return other_node.text[1:]
+
 
 if __name__ == "__main__":
     defineWord("league")
