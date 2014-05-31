38fab67f006eb71ed7219bd763b13abdf7993ac3
Spencer Elliott <me@elliottsj.com>
2014-05-31 16:35:23 -0400

    Respond with sms body

--- txti.py
+++ txti.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    from_number = request.values.get('From', None)
+    body = request.values.get('Body', None)
     resp = twilio.twiml.Response()
-    resp.message(repr(request))
+    resp.message(body)
     return str(resp)
 
 if __name__ == '__main__':
