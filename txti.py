ec6d410c47439282eaca2e0cff748733d720c852
Spencer Elliott <me@elliottsj.com>
2014-05-31 16:07:21 -0400

    Dump request to response

--- txti.py
+++ txti.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 import twilio.twiml
+
+from parser import parse
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(repr(request))
     return str(resp)
 
 if __name__ == '__main__':
