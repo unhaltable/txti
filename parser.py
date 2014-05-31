a64b938e9f2c5ead57a0242853fb6d3697dcb9d4
Jonathan Webb <jwebbed@outlook.com>
2014-05-31 17:47:17 -0400

    shit works /shrug

--- parser.py
+++ parser.py
@@ -9,12 +9,21 @@ class Parser:
         for formula in self.formulae:
             mat = re.search(formula.pieces[0], input)
             if(mat != None and mat.pos == 0):
-                self.analyze(formula, input)
-                return
+                return self.analyze(formula, input)
+
 
     def analyze(self, formula, input):
         inputs = []
-        pass
+        i = 0
+        while i < len(formula.pieces) - 1:
+            begin = re.search(formula.pieces[i], input)
+            end = re.search(formula.pieces[i + 1], input)
+            num1 = begin.end(0)
+            num2 = end.start(0)
+            inputs.append(input[num1:num2])
+            i+=1
+        inputs.append(input[end.end():])
+        return formula.f(inputs)
 
     def addFormula(self, formula):
         self.formulae.append(formula)
@@ -28,7 +37,7 @@ class Formula:
         l = re.split(r"{{|}}", formula)
         while '' in l:
             l.remove('')
-        self.pieces = l
+        self.pieces = [l[i] for i in range(0, len(l), 2)]
         self.head = l[0]
 
 
