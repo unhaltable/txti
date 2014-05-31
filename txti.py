bbcb411e2ef87a02aa3056812b8db9e5d89bd228
Spencer Elliott <me@elliottsj.com>
2014-05-31 16:42:49 -0400

    Scaffolding for parsing query and returning sms response

--- txti.py
+++ txti.py
@@ -6,12 +6,26 @@ from parser import parse
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def txti():
+    # Get parameters from request
     from_number = request.values.get('From', None)
     body = request.values.get('Body', None)
+
+    # Create the response string by parsing the query, calling relevant APIs, and returning a string
+    response = get_response(body)
+
+    # Create the response to be sent back to the user
     resp = twilio.twiml.Response()
     resp.message(body)
     return str(resp)
+
+
+def get_response(query):
+    request_parts = parse(query)
+    request_type = request_parts[0]
+
+    # TODO: Instead of returning the raw query, call a function to get data corresponding to the request type
+    return query
 
 if __name__ == '__main__':
     app.run(debug=True)
