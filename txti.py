161a4258858ad221543edbc8ce197ef33abfd2dc
Spencer Elliott <me@elliottsj.com>
2014-05-31 14:24:14 -0400

    Add twilio test

--- txti.py
+++ txti.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import twilio.twiml
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', )
 def hello_world():
-    return 'Hello World!'
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
