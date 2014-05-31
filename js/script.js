e0c64bcefed38c4a5db36aad6255b88e9ed32afb
Eugene Cheung <ey.cheung@outlook.com>
2014-05-31 17:04:59 -0400

    Phone stuff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 var scrollTo = function(el, off){
-  $('html, body').animate({
+  $("html, body").animate({
     scrollTop: $(el).offset().top - off
   }, 650);
 }
 
 $("#more-button").click(function(){ scrollTo("#learn-more", 0); });
+
+for (var i = 1; i < 3; i++) {
+  $("#messages").prepend("<li><img src='http://unhaltable.github.io/txti/img/to" + i + ".jpg' width='258px'></li>");
+  $("#messages").prepend("<li><img src='http://unhaltable.github.io/txti/img/from" + i + ".jpg' width='258px'></li>");
+}
