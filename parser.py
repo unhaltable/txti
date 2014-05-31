8002bdbded6de10a7175baab831cffe96858a212
Jonathan Webb <jwebbed@outlook.com>
2014-05-31 14:41:05 -0400

    shit happened

--- parser.py
+++ parser.py
@@ -8,18 +8,43 @@ class Parser:
         self.formulae = {} ## dict of string to Formula objects
         self.lex = [] ## list of lexical rules
 
+
     def addFormula(self, formula):
         if (self.__checkIfIDUsed(formula.id)):
             raise DuplicateIDException
         else:
             self.formulae[formula.id] = formula
-
+            for rule in formula.rules:
+                self.lex.append(rule)
 
     def __checkIfIDUsed(self, id):
         for key in self.formulae.keys():
             if id == key:
                 return True
         return False
+
+    def parse(self, input):
+        new = self.lex[:]
+        #new.append((Rep(Range("AZaz") | Range("09") | Str(" ")), TEXT))
+        lex = Lexicon(new)
+        s = cStringIO.StringIO(input)
+        scanner = Scanner(lex, s)
+
+        id = None
+        inputs = []
+        while 1:
+            token = scanner.read()
+            if token[1] is '':
+                break
+            else:
+                if (token[0][:3] == 'id_'):
+                    if (not id):
+                        id = token[0][3:]
+                    elif (id and id != token[0][3:]):
+                        raise Exception
+
+                else:
+                    inputs.append(token[0])
 
 
 class Formula:
@@ -28,7 +53,7 @@ class Formula:
         '''
         :param id: str
         :param formula: str
-        :param f: str
+        :param f: a function that takes a list of strings
         :return: None
         '''
 
@@ -55,19 +80,19 @@ class Formula:
                 break
             else:
                 tokens.append(token)
+                print token
 
         lexl = []
         param = False
         for instance in tokens:
-            if instance[0] == 'terminal':
-                if (param):
-                    lexl.append((instance[1], self.id + '_terminal'))
-                else:
-                    lexl.append((instance[1], self.id + '_id'))
+            if (instance[0] == 'terminal' and not param):
+                lexl.append((Str(instance[1]), 'id_' + self.id))
             elif(instance[0] == 'open'):
                 param = True
             elif(instance[0] == 'close'):
                 param = False
+
+        self.rules = lexl
 
         for x in lexl:
             print x
@@ -79,4 +104,7 @@ def testfunc():
     print "x"
 
 if __name__ == '__main__':
-    Formula('testfunc', 'Next bus for {{route}} at {{intersection}}', testfunc)
+    y = Formula('testfunc', 'Next bus for {{route}} at {{intersection}}', testfunc)
+    x = Parser()
+    x.addFormula(y)
+    x.parse("Next bus for 116 at Coronation and Lawrence")
