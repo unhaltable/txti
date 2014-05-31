61111063d927d71b4c3d570fd7725e41af2b3ca3
Jonathan Webb <jwebbed@outlook.com>
2014-05-31 16:56:25 -0400

    fuckthisshit

--- parser.py
+++ parser.py
@@ -1,2 +1,30 @@
 from __future__ import unicode_literals, print_function
 from pypeg2 import *
+
+class _Parameter(str):
+    grammar = re.compile(r"[\w\s]+")
+
+class _Structure(str):
+    grammar = re.compile(r"[\w\s]+")
+
+class _Formula(List):
+    grammar = maybe_some(_Structure, "{{", _Parameter, "}}")
+
+
+class Parser:
+
+    def __init__(self):
+        pass
+
+    def __parse__(self, input):
+        pass
+
+class Formula:
+
+    def __init__(self, id, formulae, f):
+        self.id  = id
+        self.f
+
+
+if __name__ == "__main__":
+    f = parse("Next bus for {{route}} at {{intersection}}", _Formula)
