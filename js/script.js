3d52feeddf3f14865fb1b5359c265037366e7983
Eugene Cheung <ey.cheung@outlook.com>
2014-05-31 17:24:55 -0400

    Updated PayPal button/footer content/phone example. Just add more queries later.

--- js/script.js
+++ js/script.js
@@ -5,8 +5,3 @@ var scrollTo = function(el, off){
 }
 
 $("#more-button").click(function(){ scrollTo("#learn-more", 0); });
-
-for (var i = 1; i < 3; i++) {
-  $("#messages").prepend("<li><img src='http://unhaltable.github.io/txti/img/to" + i + ".jpg' width='258px'></li>");
-  $("#messages").prepend("<li><img src='http://unhaltable.github.io/txti/img/from" + i + ".jpg' width='258px'></li>");
-}
