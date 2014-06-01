161a0bd9a3652d5e7b52f25da434ec385f26230c
Eugene Cheung <ey.cheung@outlook.com>
2014-05-31 23:10:08 -0400

    Weather patterns

--- initializer.py
+++ initializer.py
@@ -3,6 +3,9 @@ from api.rng import rng
 from api.jeffwu import jeff_wu_as_a_service
 from api.cdf import get_lab
 from api.four_chan import four_chan
+from api.weather import weather_current
+from api.weather import weather_forecast_today
+from api.weather import weather_forecast_next
 
 def getParser():
     parser = Parser()
@@ -10,6 +13,9 @@ def getParser():
     parser.addFormula(Formula("jeff_wu_as_a_service", "Jeff Wu", jeff_wu_as_a_service))
     parser.addFormula(Formula("cdf", "CDF {{lab}}", get_lab))
     parser.addFormula(Formula("4chan", "4chan {{board}}", four_chan))
+    parser.addFormula(Formula("weather_current", "Current weather in {{city}}", weather_current))
+    parser.addFormula(Formula("weather_today", "Today's forecast for {{city}}", weather_forecast_today))
+    parser.addFormula(Formula("weather_forecast", "Forecast for {{city}}", weather_forecast_next))
 
     return parser
 
