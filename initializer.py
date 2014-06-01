4fce58ecfb403b5bbee8beb4284265a5935722fb
Eugene Cheung <ey.cheung@outlook.com>
2014-05-31 21:51:21 -0400

    CDF

--- initializer.py
+++ initializer.py
@@ -1,12 +1,13 @@
 from parser import *
 from api.rng import rng
 from api.jeffwu import jeff_wu_as_a_service
+from api.cdf import get_lab
 
 def getParser():
     parser = Parser()
     parser.addFormula(Formula("rng", "rng{{minmax}}", rng))
     parser.addFormula(Formula("jeff_wu_as_a_service", "Jeff Wu", jeff_wu_as_a_service))
-
+    parser.addFormula(Formula("cdf", "CDF {{lab}}", get_lab))
 
     return parser
 
