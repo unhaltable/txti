420d5e536f9d9ca6b1db061dee1b8f97c03a9add
Eugene Cheung <ey.cheung@outlook.com>
2014-06-01 04:41:57 -0400

    Supah ghetto bus predictions, nigga

--- api/nextbus.py
+++ api/nextbus.py
@@ -1,15 +1,50 @@
+import urllib2
 import xml.etree.ElementTree as ET
-import requests
 
 nextbus_route_config = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r={}'
+nextbus_stop_config = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictionsForMultiStops&a=ttc&stops={}%7C{}'
+
+
+def parse_xml(file, tag_name, attr1, attr2):
+    f = urllib2.urlopen(file)
+    xml_string = f.read()
+    f.close()
+    return ET.fromstring(xml_string)
+
+
+def get_stop(stops, intersection):
+    entry = stops[0]
+    for node in entry:
+        if node.tag == 'stop':
+            title = node.attrib['title'].split(' At ')
+            if intersection[1:] == title[:-1] or intersection == title:
+                return node.attrib['tag']
+    return -1
+
 
 def get_bus_prediction(params):
     route_tag = params[0]
     direction = params[1]
-    intersection = params[2]
-    route_config = requests.get(nextbus_route_config.format(route_tag))
+    intersection = params[2] # "College St at St George St"
 
-    tree = ET.fromstring(route_config.content)
-    # TODO: parse xml tree
+    stops = parse_xml(nextbus_route_config.format(route_tag), 'stop', 'tag', 'title')
+    stop_id = get_stop(stops, intersection.split(' at '))
 
-    return 'Next bus for {} {} at {}'.format(route_tag, direction, intersection)
+    if stop_id == -1:
+        return 'Could not find stop'
+
+    predictions = parse_xml(nextbus_stop_config.format(route_tag, stop_id), 'prediction', 'seconds', 'minutes')
+    try:
+        first_prediction = list(predictions)[0][0][0].attrib
+        minutes = first_prediction['minutes']
+        seconds = first_prediction['seconds']
+        actual_seconds = int((int(seconds)/60.0) % 1 * 60)
+        if actual_seconds < 10: actual_seconds = '0' + str(actual_seconds)
+
+        return 'Next bus for {} {} at {} is coming in {}m and {}s.'.format(route_tag, direction, intersection, minutes, actual_seconds)
+    except:
+        return 'No predictions available'
+
+
+if __name__ == '__main__':
+    print get_bus_prediction(['310', 'North', 'Bathurst St at Dewlane Dr'])
--- initializer.py
+++ initializer.py
@@ -4,6 +4,7 @@ from api.cdf import lab
 from api.dictionary import define, get_definition
 from api.four_chan import four_chan
 from api.jeffwu import jeff_wu_as_a_service
+from api.nextbus import get_bus_prediction
 from api.rng import rng
 from api.rotten import get_movie
 from api.weather import get_conditions, get_forecast_today, get_forecast
@@ -16,6 +17,7 @@ def get_parser():
     parser.addFormula(Formula("cdf", "CDF {{lab}}", lab))
     parser.addFormula(Formula("dictionary", "Define {{word}}", define))
     parser.addFormula(Formula("jeff_wu_as_a_service", "Jeff Wu", jeff_wu_as_a_service))
+    parser.addFormula(Formula("next_bus", "Next bus for {{route}} {{direction}} at {{intersection}}", get_bus_prediction))
     parser.addFormula(Formula("rng", "rng{{minmax}}", rng))
     parser.addFormula(Formula("rotten_tomatoes", "Rotten {{movie}}", get_movie))
     parser.addFormula(Formula("weather_current", "Current weather in {{city}}", get_conditions))
