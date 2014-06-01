49eeefa898989bf9781eaf77de733067bb47046e
Eugene Cheung <ey.cheung@outlook.com>
2014-06-01 01:21:10 -0400

    Bitcoin

--- initializer.py
+++ initializer.py
@@ -1,23 +1,24 @@
 from parser import *
-from api.cdf import get_lab
-from api.dictionary import get_definition
+from api.btc import get_btc_info
+from api.cdf import lab
+from api.dictionary import define, get_definition
 from api.four_chan import four_chan
 from api.jeffwu import jeff_wu_as_a_service
 from api.rng import rng
-from api.weather import weather_current
-from api.weather import weather_forecast_next
-from api.weather import weather_forecast_today
+from api.weather import get_conditions, get_forecast_today, get_forecast
+
 
 def get_parser():
     parser = Parser()
     parser.addFormula(Formula("4chan", "4chan {{board}}", four_chan))
-    parser.addFormula(Formula("cdf", "CDF {{lab}}", get_lab))
-    parser.addFormula(Formula("dictionary", "Define {{word}}", get_definition))
+    parser.addFormula(Formula("btc", "BTC", get_btc_info))
+    parser.addFormula(Formula("cdf", "CDF {{lab}}", lab))
+    parser.addFormula(Formula("dictionary", "Define {{word}}", define))
     parser.addFormula(Formula("jeff_wu_as_a_service", "Jeff Wu", jeff_wu_as_a_service))
     parser.addFormula(Formula("rng", "rng{{minmax}}", rng))
-    parser.addFormula(Formula("weather_current", "Current weather in {{city}}", weather_current))
-    parser.addFormula(Formula("weather_forecast", "Forecast for {{city}}", weather_forecast_next))
-    parser.addFormula(Formula("weather_today", "Today's forecast for {{city}}", weather_forecast_today))
+    parser.addFormula(Formula("weather_current", "Current weather in {{city}}", get_conditions))
+    parser.addFormula(Formula("weather_forecast", "Forecast for {{city}}", get_forecast))
+    parser.addFormula(Formula("weather_today", "Today's forecast for {{city}}", get_forecast_today))
 
     return parser
 
