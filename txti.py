884b773f3663363bd61660ff859096e6c22bf12d
Spencer Elliott <me@elliottsj.com>
2014-06-01 10:48:40 -0400

    Fix

--- txti.py
+++ txti.py
@@ -223,4 +223,4 @@ if __name__ == '__main__':
     log.setLevel(logging.INFO)
     log.addHandler(logging.StreamHandler(sys.stdout))
 
-    app.run(debug=True, PORT=os.environ.get('PORT'))
+    app.run(debug=True, port=os.environ.get('PORT'))
