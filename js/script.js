deaea59ca999a680a354e2864239a604319cade1
Eugene Cheung <ey.cheung@outlook.com>
2014-05-31 22:16:05 -0400

    Phone stuff

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ var scrollTo = function(el, off){
 }
 
 $("#more-button").click(function(){ scrollTo("#learn-more", 0); });
+
+$(".recipe").click(function() {
+  var image = "img/phone/" + $(this).attr("data-key") + ".jpg";
+  $("#example").attr('src', image);
+});
