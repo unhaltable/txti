3e34849fb36621568a8e1ec270c2a4be9263c2e9
Eugene Cheung <ey.cheung@outlook.com>
2014-06-01 12:01:00 -0400

    MINIFY

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,1 @@
-var scrollTo = function(el, off){
-  $("html, body").animate({
-    scrollTop: $(el).offset().top - off
-  }, 650);
-}
-
-$("#more-button").click(function(){ scrollTo("#learn-more", 0); });
-
-$(".recipe").click(function() {
-  var image = "static/img/phone/" + $(this).attr("data-key") + ".jpg";
-  $("#example").css('background-image', 'url(/' + image + ')');
-});
-
+var scrollTo=function(a,b){$("html, body").animate({scrollTop:$(a).offset().top-b},650)};$("#more-button").click(function(){scrollTo("#learn-more",0)}),$(".recipe").click(function(){var a="static/img/phone/"+$(this).attr("data-key")+".jpg";$("#example").css("background-image","url(/"+a+")")});
