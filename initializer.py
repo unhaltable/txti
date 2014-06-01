df86a7d5832c41d85ac9a1438274a3aeca6b7460
Eugene Cheung <ey.cheung@outlook.com>
2014-06-01 02:05:08 -0400

    Rotten tomatoes

--- initializer.py
+++ initializer.py
@@ -5,6 +5,7 @@ from api.dictionary import define, get_definition
 from api.four_chan import four_chan
 from api.jeffwu import jeff_wu_as_a_service
 from api.rng import rng
+from api.rotten import get_movie
 from api.weather import get_conditions, get_forecast_today, get_forecast
 
 
@@ -16,6 +17,7 @@ def get_parser():
     parser.addFormula(Formula("dictionary", "Define {{word}}", define))
     parser.addFormula(Formula("jeff_wu_as_a_service", "Jeff Wu", jeff_wu_as_a_service))
     parser.addFormula(Formula("rng", "rng{{minmax}}", rng))
+    parser.addFormula(Formula("rotten_tomatoes", "Rotten {{movie}}", get_movie))
     parser.addFormula(Formula("weather_current", "Current weather in {{city}}", get_conditions))
     parser.addFormula(Formula("weather_forecast", "Forecast for {{city}}", get_forecast))
     parser.addFormula(Formula("weather_today", "Today's forecast for {{city}}", get_forecast_today))
